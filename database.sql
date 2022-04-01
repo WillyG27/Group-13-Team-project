b2424c7cbd729fbef48b590ce2c5cfc1363b826b
AaronKrns <73694811+AaronKrns@users.noreply.github.com>
2022-04-01 11:27:46 +0100

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS arable_land_manager;
+CREATE DATABASE arable_land_manager;
+USE arable_land_manager;
+
+CREATE TABLE field (
+	field_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	field_name VARCHAR(255),
+    size VARCHAR(255),
+    soil_type VARCHAR(255)
+);
+
+CREATE TABLE crop_activity (
+	crop_activity_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	start_date VARCHAR(255),
+	crop VARCHAR(255),
+    field_id INTEGER NOT NULL,
+    FOREIGN KEY (field_id) REFERENCES field (field_id) ON DELETE CASCADE
+);
