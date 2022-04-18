88c09a5f5a6c4e1480a10d6c8c07543cd10cd31f
AaronKrns <73694811+AaronKrns@users.noreply.github.com>
2022-04-18 16:51:42 +0100

    Update field.js

--- field.js
+++ field.js
@@ -1,170 +1,49 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mysql=require('mysql');
+var MySql = require('sync-mysql');
+var connection_info = require("../modules/connection_info")
 
 
 
-/* GET home page.
-router.get('/field', function(req, res, next) {
-  res.render('field');
-
-});
-*/
-
- //testing db connection
-/*
- var con=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'2002',
-    database:'arable_land_manager'
- });
-
- con.connect(function(err){
-    if(err){
-      console.log(err);
-    }
-    else{
-     con.query("SELECT * FROM field " ,function(err,result){
-          var that=result;
-
-          if (err) {
-              console.log(err);
-          }
-          else {
-              console.log(that);
-
-              console.log('this done')
-          }
-
-
-      })
-      }
- });
-
-*/
-
-
-
-//display info
-
-router.get('/field', function(req, res, next) {
-  var con=mysql.createConnection({
-     host:'localhost',
-     user:'root',
-     password:'2002',
-     database:'arable_land_manager'
+/*  router.get('/', ... used for loading the field page so the add_field page can be navigated to */
+router.get('/', function(req, res, next) {
+  var connection = new MySql({
+    host: connection_info.host,
+    user: connection_info.user,
+    password: connection_info.password,
+    database: connection_info.database
   });
-  con.connect(function(err){
-     if(err){
-       console.log(err);
-     }
-     else{
+  var field = connection.query("SELECT * from field");
+  console.log(field);
 
-       con.query("SELECT * FROM field ",function(err,result){
-
-         var field=result;
-
-              console.log('contact');
-              console.log(field);
-               res.render('field',{field:field});
-
-        });
-      }
-  });
+  res.render('field', { title: 'Express', field:field });
 });
 
 
 
-//edit info
-router.get('/edit',function(req,res){
-    var field_id=req.query.field_id
-    console.log(field_id);
+
+router.get('/add', function(req, res, next){
+
+  res.render('add_field')
+})
 
 
-    var con=mysql.createConnection({
-       host:'localhost',
-       user:'root',
-       password:'2002',
-       database:'arable_land_manager'
-    });
+router.post('/add', function(req, res, next) {
+  var field_id = parseFloat(req.body.field_id)
+  var size = req.body.size
+  var soil_type = req.body.soil_type
+  var connection = new MySql({
+    host: connection_info.host,
+    user: connection_info.user,
+    password: connection_info.password,
+    database: connection_info.database
+  })
+  connection.query("INSERT INTO field (field_id, size, soil_type) VALUES ((?), (?), (?));", [field_id, size, soil_type]);
 
-    con.connect(function(err){
-       if(err){
-         console.log(err);
-       }
-       else{
-
-         con.query("SELECT * FROM field ",function(err,result){
-
-           var field=result;
-
-                console.log('contact');
-                console.log(field);
-
-                res.render("edit",{field_id:field_id, field:field});
+  res.redirect("/field");
+})
 
 
 
-          });
-        }
-
-    });
-
-
-});
-
-router.post('/edit', function(req,res,next){
-  var field_id=req.body.field_id;
-  var size=req.body.size;
-  var soil_type=req.body.soil_type;
-
-    var con=mysql.createConnection({
-       host:'localhost',
-       user:'root',
-       password:'2002',
-       database:'arable_land_manager'
-    });
-
-
-
-
-         con.query("UPDATE field SET size=(?) ,soil_type=(?) WHERE field_id=(?)",[size, soil_type, field_id]);
-
-         con.query("SELECT * FROM field ",function(err,result){
-
-           var field=result;
-         res.render('field', {field:field});
-         });
-
-        });
-
-
-
-
-//remove info
-router.post("/delete", function(req,res,next){
-  var con=mysql.createConnection({
-     host:'localhost',
-     user:'root',
-     password:'2002',
-     database:'arable_land_manager'
-  });
-
-  var delete_id =req.body.field_id;
-  console.log(delete_id);
-  con.query("DELETE from field where field_id=(?);",[delete_id]);
-
-  con.query("SELECT * FROM field ",function(err,result){
-
-    var field=result;
-  res.render('field', {field:field});
-});
-});
 
 module.exports = router;
-
-
-
-
-    
